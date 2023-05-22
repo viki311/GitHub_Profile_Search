fe267e88bcdf7188350de4841e72a3415c9511e4
viki311 <viki966waran4@gmail.com>
2023-05-22 20:17:15 +0530

    Final Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const App = () => {
+  let [name, setName] = useState("");
+  let [user, setUser] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${name}`)
+      .then((res) => {
+        console.log(res.status);
+        setUser(res.data);
+      })
+      .catch((err) => console.log(err));
+    setUser(null);
+  }, [name]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      {user == null ? (
+        <h1>User Not Found</h1>
+      ) : (
+        <div>
+          <h1>Name: {user.name}</h1>
+          <img src={user.avatar_url} style={{ width: "200px" }} />
+          <h1>Followers: {user.followers}</h1>
+          <h1>Following: {user.following}</h1>
+          <a href={user.html_url} target="_blank">
+            Link to my account
+          </a>
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
